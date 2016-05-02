995ab0a97bba79c660d1215c55d136893a6d9090
curcuz <czkzcz.cmc@gmail.com>
2016-05-02 15:01:44 +0200

    axiss hel

--- app/js/diddyborg.service.js
+++ app/js/diddyborg.service.js
@@ -1,13 +1,13 @@
 function DiddyborgService() {
   'use strict';
-  var socket = io();
+  var socket = io('192.168.178.49');
   function motorRight(gaugeRight,token) {
     socket.emit('motor1', gaugeRight, token);
-    return gaugeRight;
+    return -gaugeRight;
   }
   function motorLeft(gaugeLeft,token) {
     socket.emit('motor2', gaugeLeft,token);
-    return gaugeLeft;
+    return -gaugeLeft;
   }
 
   return {
